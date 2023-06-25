4a17ddac10682566857e2ba60e48fae181402afa
Eanano <Eanan12@gmail.com>
2023-06-25 18:33:47 +0100

    Basic authentication added

--- API/Data/Migrations/20230620210448_UserPasswordAdded.Designer.cs
+++ API/Data/Migrations/20230620210448_UserPasswordAdded.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20230619212640_InitialCreateAgain")]
-    partial class InitialCreateAgain
+    [Migration("20230620210448_UserPasswordAdded")]
+    partial class UserPasswordAdded
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -24,6 +24,12 @@ namespace API.Data.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
+
+                    b.Property<byte[]>("PasswordHash")
+                        .HasColumnType("BLOB");
+
+                    b.Property<byte[]>("PasswordSalt")
+                        .HasColumnType("BLOB");
 
                     b.Property<string>("UserName")
                         .HasColumnType("TEXT");
